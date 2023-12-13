daa4aae3343b18b20de37fe350bb0772578ee7d6
Alexander Korobov <dktwho@mail.ru>
2023-12-13 19:38:42 +0300

    feat:change route to createBrowserRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import {Button} from "./components/Button/Button.tsx";
 import {useState} from "react";
 import {Input} from "./components/Input/Input.tsx";
-import {Route, Routes} from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import {Card} from "./pages/Card/Card.tsx";
 import {Menu} from "./pages/Menu/Menu.tsx";
 import {Error} from "./pages/Error/Error.tsx";
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Menu/>
+    },
+    {
+        path: '/card',
+        element: <Card/>
+    },
+    {
+        path: '*',
+        element: <Error/>
+    },
+])
+
 function App() {
     const [counter, setCounter] = useState<number>(0)
 
-  return (
-    <>
-        <div>{counter}</div>
-        <Button onClick={()=> setCounter(prev => prev + 1)}>Some button</Button>
-        <Button appearance={'big'}  onClick={()=> setCounter(prev => prev + 1)}>Some button</Button>
-        <Input placeholder={'email'}/>
-        <div>
-            <a href="/">Menu</a>
-            <a href="/card">Card</a>
-        </div>
-        <Routes>
-            <Route path={'/'} element={<Menu/>}/>
-            <Route path={'/card'} element={<Card/>}/>
-            <Route path={'*'} element={<Error/>}/>
-
-
-        </Routes>
-    </>
-  )
+    return (
+        <>
+            <div>{counter}</div>
+            <Button onClick={() => setCounter(prev => prev + 1)}>Some button</Button>
+            <Button appearance={'big'} onClick={() => setCounter(prev => prev + 1)}>Some button</Button>
+            <Input placeholder={'email'}/>
+            <div>
+                <a href="/">Menu</a>
+                <a href="/card">Card</a>
+            </div>
+            <RouterProvider router={router}/>
+        </>
+    )
 }
 
 export default App
