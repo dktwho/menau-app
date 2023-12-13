f75e5460f6a92ca38e104193564157752671765d
Alexander Korobov <dktwho@mail.ru>
2023-12-13 19:30:22 +0300

    feat:add two Route test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {Button} from "./components/Button/Button.tsx";
 import {useState} from "react";
 import {Input} from "./components/Input/Input.tsx";
+import {Route, Routes} from "react-router-dom";
+import {Card} from "./pages/Card/Card.tsx";
+import {Menu} from "./pages/Menu/Menu.tsx";
 
 function App() {
     const [counter, setCounter] = useState<number>(0)
@@ -11,6 +14,16 @@ function App() {
         <Button onClick={()=> setCounter(prev => prev + 1)}>Some button</Button>
         <Button appearance={'big'}  onClick={()=> setCounter(prev => prev + 1)}>Some button</Button>
         <Input placeholder={'email'}/>
+        <div>
+            <a href="/">Menu</a>
+            <a href="/card">Card</a>
+        </div>
+        <Routes>
+            <Route path={'/'} element={<Menu/>}/>
+            <Route path={'/card'} element={<Card/>}/>
+
+
+        </Routes>
     </>
   )
 }
