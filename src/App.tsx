b9533deed3398b65bdbe16a9d39b24d178402c29
Alexander Korobov <dktwho@mail.ru>
2023-12-13 11:19:11 +0300

    feat: test useState type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import {Button, ButtonAlt} from "./components/Button/Button.tsx";
+import {useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState<number>(0)
 
   return (
     <>
-        <Button onClick={()=> console.log('btn')}>Some button</Button>
+        <div>{counter}</div>
+        <Button onClick={()=> setCounter(prev => prev + 1)}>Some button</Button>
         <ButtonAlt onClick={()=> console.log('btnalt')}>Some button Alt</ButtonAlt>
     </>
   )
