90774582c4bd1acaf7365b190aed4041c5ba8742
Alexander Korobov <dktwho@mail.ru>
2023-12-14 12:07:32 +0300

    feat:add change useLocation to NavLink with args isActive

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,16 +1,9 @@
-import {Link, Outlet, useLocation} from "react-router-dom";
+import {NavLink, Outlet} from "react-router-dom";
 import styles from './Layout.module.css'
 import {Button} from "../../components/Button/Button.tsx";
-import {useEffect} from "react";
 import cn from 'classnames'
 
 export const Layout = () => {
-    const location = useLocation()
-
-    useEffect(() => {
-        console.log(location)
-    }, [location])
-
     return (
         <div className={styles['layout']}>
             <div className={styles['sidebar']}>
@@ -20,19 +13,20 @@ export const Layout = () => {
                     <div className={styles['email']}>user@mail.com</div>
                 </div>
                 <div className={styles['menu']}>
-                    <Link to="/" className={cn(styles['link'], {
-                        [styles.active]: location.pathname === '/'
+                    <NavLink to="/" className={({isActive}) => cn(styles['link'], {
+                        [styles.active]: isActive
                     })}>
-                        <img src="/menu-icon.svg" alt="menu"/>Menu</Link>
-                    <Link to="/card" className={styles['link']}>
-                        <img src="/cart-icon.svg" alt="cart"/>Card</Link>
+                        <img src="/menu-icon.svg" alt="menu"/>Menu</NavLink>
+                    <NavLink to="/card" className={({isActive})=> cn(styles['link'], {
+                        [styles.active]: isActive
+                    })}>
+                        <img src="/cart-icon.svg" alt="cart"/>Card</NavLink>
                 </div>
                 <Button className={styles['exit']}>
                     <img src="/exit-icon.svg" alt="exit-menu"/>Exit
                 </Button>
             </div>
             Menu
-
             <div>
                 <Outlet/>
             </div>
