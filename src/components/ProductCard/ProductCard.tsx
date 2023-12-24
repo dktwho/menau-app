1e6033d4c27dc95299246e35239af3e94f5fd95e
Alexander Korobov <dktwho@mail.ru>
2023-12-24 16:58:13 +0300

    feat:example hardcode one ProductCart with style and landing

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,25 +1,30 @@
 import styles from './ProductCard.module.css'
 import {ProductCardProps} from "./ProductCard.props.ts";
+import {Link} from "react-router-dom";
 
 export const ProductCard = ({id, title, price, rating, image, description}: ProductCardProps) => {
     return (
-        <div className={styles['card']}>
-            <div className={styles['head']}>
-                <div className={styles['price']}>
-                    {price}
+        <Link to={'#'}>
+            <div className={styles['card']}>
+                <div className={styles['head']} style={{backgroundImage: `url('${image}')`}}>
+                    <div className={styles['price']}>
+                        {price}&nbsp;
+                        <span className={styles['currency']}>₽</span>
+                    </div>
+                    <button className={styles['add-to-cart']}>
+                        <img src="/add-to-cart-icon.svg" alt="add-to-cart"/>
+                    </button>
+                    <div className={styles['rating']}>
+                        {rating}&nbsp;
+                        <img src="/rating-star.svg" alt="rating-star"/>
+                    </div>
                 </div>
-                <button className={styles['add-to-cart']}>
-                    <img src="/add-to-cart-icon.svg" alt="add-to-cart"/>
-                </button>
-                <div className={styles['rating']}>
-                    {rating}
-                    <img src="/rating-star.svg" alt="rating-star"/>
+                <div className={styles['footer']}>
+                    <div className={styles['title']}>{title}</div>
+                    <div className={styles['description']}>{description}</div>
                 </div>
             </div>
-            <div className={styles['footer']}>
-                <div className={styles['title']}>{title}</div>
-                <div className={styles['description']}>{description}</div>
-            </div>
-        </div>
+        </Link>
+
     );
 };
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import {Headling} from "../../components/Headling/Headling.tsx";
 import {Search} from "../../components/Search/Search.tsx";
 import styles from './Menu.module.css'
+import {ProductCard} from "../../components/ProductCard/ProductCard.tsx";
 
 export const Menu = () => {
     return (
@@ -8,6 +9,9 @@ export const Menu = () => {
             <div className={styles['head']}>
                 <Headling>Menu</Headling>
                 <Search placeholder={'Введите блюдо или состав'}></Search>
+            </div>
+            <div>
+                <ProductCard id={1} title={'Наслаждение'} description={'Салями, руккола, помидоры, оливки'} image='/product-demo.png' price={300} rating={4.5}/>
             </div>
 
         </>
