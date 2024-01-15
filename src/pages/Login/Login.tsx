14f03a7d05e5cfed5c2ed13f57732c289ab25d1c
Alexander Korobov <dktwho@mail.ru>
2024-01-15 17:31:13 +0300

    feat: add style for Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,30 +2,31 @@ import {Headling} from "../../components/Headling/Headling.tsx";
 import {Input} from "../../components/Input/Input.tsx";
 import {Button} from "../../components/Button/Button.tsx";
 import {Link} from "react-router-dom";
+import styles from './Login.module.css'
 
 export const Login = () => {
     return (
-        <div>
+        <div className={styles['login']}>
             <Headling>Enter</Headling>
-            <form>
-                <div>
+            <form className={styles['form']}>
+                <div className={styles['field']}>
                     <label htmlFor="">
                         Your email
-                        <Input id={'email'} placeholder={'type here your email'} />
+                        <Input id={'email'} placeholder={'type here your email'}/>
                     </label>
                 </div>
-                <div>
+                <div className={styles['field']}>
                     <label htmlFor="">
                         Your password
-                        <Input id={'password'} type={'password'} placeholder={'type here your password'}  />
+                        <Input id={'password'} type={'password'} placeholder={'type here your password'}/>
                     </label>
                 </div>
                 <Button appearance={'big'}>Enter</Button>
-                <div>Do you have account ?</div>
-                <div>
-                    <Link to={'/auth/register'}></Link>
-                </div>
             </form>
+            <div>
+                <div>Do you have account ?</div>
+                <Link to={'/auth/register'}>Register now</Link>
+            </div>
         </div>
     );
 };
