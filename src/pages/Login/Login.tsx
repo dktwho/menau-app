3ff04c216b7b256c27c1d6be1baaca221f11bd8f
Alexander Korobov <dktwho@mail.ru>
2024-01-16 17:15:19 +0300

    feat: add sendLogin with axios post, add try catch, check access token

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,27 +4,58 @@ import {Button} from "../../components/Button/Button.tsx";
 import {Link} from "react-router-dom";
 import styles from './Login.module.css'
 import {FormEvent} from "react";
+import axios, {AxiosError} from "axios";
+import {PREFIX} from "../../helpers/api.ts";
+
+export type LoginFormType = {
+    email: {
+        value: string
+    }
+    password: {
+        value: string
+    }
+}
 
 export const Login = () => {
-    const onSubmitHandler = (e: FormEvent) => {
+
+
+    const onSubmitHandler = async (e: FormEvent) => {
         e.preventDefault()
-        console.log(e)
+        const target = e.target as typeof e.target & LoginFormType;
+        const {email, password} = target;
+        await sendLogin(email.value, password.value)
+    }
+
+    const sendLogin = async(email: string, password: string) => {
+        try {
+            const { data } = await axios.post(`${PREFIX}/auth/login`, {
+                email,
+                password
+            })
+            console.log(data)
+        } catch (e) {
+            if(e instanceof AxiosError) {
+                console.log(e.message)
+            }
+        }
+
     }
 
     return (
-        <div className={styles['login']} onSubmit={onSubmitHandler}>
+        <div className={styles['login']}>
             <Headling>Enter</Headling>
-            <form className={styles['form']}>
+            <form className={styles['form']} onSubmit={onSubmitHandler}>
                 <div className={styles['field']}>
                     <label htmlFor="">
                         Your email
-                        <Input id={'email'} placeholder={'type here your email'}/>
+                        <Input id={'email'} placeholder={'type here your email'} name={'email'}/>
                     </label>
                 </div>
                 <div className={styles['field']}>
                     <label htmlFor="">
                         Your password
-                        <Input id={'password'} type={'password'} placeholder={'type here your password'}/>
+                        <Input id={'password'} type={'password'} name={'password'}
+                               placeholder={'type here your password'}/>
                     </label>
                 </div>
                 <Button appearance={'big'}>Enter</Button>
