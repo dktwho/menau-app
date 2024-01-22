f1476eb7588d990a795e1f572bdbedb5066605ac
Alexander Korobov <dktwho@mail.ru>
2024-01-22 19:44:55 +0300

    feat:add logic for added to cart from every menuitem, with useParam

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,11 +1,21 @@
-import {Await, useLoaderData} from "react-router-dom";
+import {Await, useLoaderData, useParams} from "react-router-dom";
 import {ProductInterface} from "../../interfaces/product.interface.ts";
-import {Suspense} from "react";
+import {MouseEvent, Suspense} from "react";
 import styles from './Product.module.css'
 import {Button} from "../../components/Button/Button.tsx";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../store/store.ts";
+import {cartActions} from "../../store/cartSlice.ts";
 
 export const Product = () => {
     const data = useLoaderData() as { data: ProductInterface };
+    const params = useParams();
+    const dispatch = useDispatch<AppDispatch>()
+    const addItem = (e: MouseEvent) => {
+        e.preventDefault()
+        dispatch(cartActions.add(Number(params.id)))
+    }
+
     return (
         <div>
             <Suspense fallback={'Loading'}>
@@ -14,15 +24,14 @@ export const Product = () => {
                         <>
                             <div className={styles['productContainer']}>
                                 <h1>{data.name} </h1>
-                                <Button className={styles['button']} appearance={'small'}>В
+                                <Button className={styles['button']} appearance={'small'} onClick={addItem}> В
                                     корзину</Button>
                             </div>
-
                             <img className={styles['productImage']} src={data.image} alt=""/>
-
                             <div className={styles['productDescription']}>
-                                <div className={styles['productPrice']}>Цена: {data.price} <span>₽</span> </div>
-                                <div className={styles['productRating']}>Рейтинг: <span>{data.rating} </span><img src="/rating-star.svg" alt=""/></div>
+                                <div className={styles['productPrice']}>Цена: {data.price} <span>₽</span></div>
+                                <div className={styles['productRating']}>Рейтинг: <span>{data.rating} </span><img
+                                    src="/rating-star.svg" alt=""/></div>
                                 <div className={styles['productItems']}>Состав: {data.ingredients.map((el) => {
                                     return (
                                         <li className={styles['productItem']} key={el + 1}>{el}</li>
@@ -30,7 +39,6 @@ export const Product = () => {
                                 })}
                                 </div>
                             </div>
-
                         </>
                     )}
                 </Await>
